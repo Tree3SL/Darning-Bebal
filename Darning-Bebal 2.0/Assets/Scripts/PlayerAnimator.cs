cc62a640117c8f6bc54bb077af879247defba85f
Tao <tying4@uci.edu>
2021-02-04 05:14:21 +0800

    Demo Map 2.0
    Door in the middle/No wall at the window position/Still need fix on floors and blocks

---
+++ Darning-Bebal 2.0/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField]
+    private Sprite[] idle;
+    [SerializeField]
+    private Sprite[] walk;
+    
+    private int currentSprite = 0;
+    private SpriteRenderer player;
+    private int animationType = 0;
+
+    #endregion
+
+    private void Start()
+    {
+        player = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        bool input = Input.anyKey;
+
+        if(!input)
+        {
+            IdleAnimation();
+        }
+        else
+        {
+            WalkAnimation(Input.GetAxis("Horizontal"));
+        }
+
+        currentSprite++;
+    }
+
+    private void IdleAnimation()
+    {
+        if(animationType != 0)
+        {
+            animationType = 0;
+            currentSprite = 0;
+        }
+        else if(currentSprite >= idle.Length)
+        {
+            currentSprite = 0;
+        }
+
+        player.sprite = idle[currentSprite];
+    }
+
+    private void WalkAnimation(float axis)
+    {
+        if(animationType != 1)
+        {
+            animationType = 1;
+            currentSprite = 0;
+        }
+        else if(currentSprite >= walk.Length)
+        {
+            currentSprite = 0; ;
+        }
+
+        player.sprite = walk[currentSprite];
+
+        if(axis < 0)
+        {
+            player.flipX = true;
+        }
+        else if(player.flipX)
+        {
+            player.flipX = false;
+        }
+    }
+}
